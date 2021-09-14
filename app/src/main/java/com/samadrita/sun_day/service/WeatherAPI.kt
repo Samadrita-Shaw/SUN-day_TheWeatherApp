54c6d0eec692ef213da9305cab0482e7e8a16aa2
Samadrita-Shaw <samadrita.shaw@s.amity.edu>
2021-09-14 09:55:15 +0530

    The Weather API app - SUN-day

--- app/src/main/java/com/samadrita/sun_day/service/WeatherAPI.kt
+++ app/src/main/java/com/samadrita/sun_day/service/WeatherAPI.kt
@@ -1,5 +1,6 @@
 package com.samadrita.sun_day.service
 
+import com.samadrita.sun_day.BuildConfig
 import com.samadrita.sun_day.model.WeatherModel
 import io.reactivex.Single
 import retrofit2.http.GET
@@ -9,7 +10,7 @@ import retrofit2.http.Query
 
 interface WeatherAPI {
 
-    @GET("data/2.5/weather?&units=metric&appid=0cce8507e62707a48e201a7436eef300")
+    @GET("data/2.5/weather?&units=metric&appid="+ BuildConfig.API_KEY)
     fun getData(
         @Query("q") cityName : String
     ): Single<WeatherModel>
